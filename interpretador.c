055e9f3c373c045e19ec814a27f408b0c9f0e1f9
marcossduarte <51235552+marcossduarte@users.noreply.github.com>
2020-11-05 23:33:49 -0300

    Update interpretador.c

--- interpretador.c
+++ interpretador.c
@@ -5,11 +5,22 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <errno.h>
+int ContarVirgulas(char *li);
+void remove_espaco(char *str);
+void linha(char *linha, char **argumentos);
 
+//================================= EXECUTAR COMANDO ==============================
 void executarComandos(char *argv[]) {
 	
 	pid_t pid = fork(); //Criou processo filho
 	int i;
+	
+	if (strcmp(argv[0], "quit") == 0) //saída do programa
+		exit(EXIT_SUCCESS);
+	
+	if (argv[0] == NULL){ // Entrada somente de espaço/virgula
+		return;
+	}
 	
 	//Erro, fork não criou o processo
 	if (pid < 0) {
@@ -48,10 +59,63 @@ void executarComandos(char *argv[]) {
 		
 	}
 }
+//================================= CONTAR VIRGULA ==============================
+int ContarVirgulas(char *li)
+{
+	int virgula = 0, i = 0;
+	while (li[i] != '\0')
+	{
+		if (li[i] == ',')
+		{
+			virgula++;
+		}
+		i++;
+	}
+	return virgula;
+}
+//================================= APAGA ESPACO EM BRANCO ==============================
+void remove_espaco(char *str) //apaga espaços em branco
+{
+	int i, j;
+	for (i = 0; i < strlen(str) - 1; i++)
+	{
+		if (str[i] == ' ' && str[i + 1] == ' ')
+		{
+			for (j = i; j < strlen(str); j++)
+			{
+				str[j] = str[j + 1];
+			}
+			i--;
+		}
+	}
+}
+
+//================================= TRATAMENTO DAS LINHAS ==============================
+void linha(char *linha, char **argumentos)
+{
+	//separa a string e armazena na variavel argumentos
+
+	remove_espaco(linha);
+	int i = 0;
+
+	char *rest = NULL;
+	char *token;
+
+	token = strtok_r(linha, " ", &rest);
+
+	while (token != NULL)
+	{
+		argumentos[i] = token;
+		//printf("token: %s\n", token);
+		token = strtok_r(NULL, " ", &rest);
+		i++;
+	}
+	argumentos[i] = NULL;
+}
 
 int main (int argc, char *argv[]) {
 	
-	teste
+	
 
 	return 0;
 }
